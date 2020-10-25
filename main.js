db9069d4d75be266a35b53786a71cdcd3f83ef8c
Lucas Sanchez <08.lucas.sanchez@gmail.com>
2020-10-25 19:19:28 +0100

    timer va au de la de 10 minutes

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let firstClick = false
 let counter = 0
 let cardPair = []
 let score = document.querySelector("#score").innerHTML
+let srcs = ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg', 'images/img4.jpg', 'images/img5.jpg', 'images/img6.jpg', 'images/img7.jpg', 'images/img8.jpg', 'images/img9.jpg', 'images/img10.jpg','images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg', 'images/img4.jpg', 'images/img5.jpg', 'images/img6.jpg', 'images/img7.jpg', 'images/img8.jpg', 'images/img9.jpg', 'images/img10.jpg'];
 
 // c'est bien juste card dans le foreach, je crois que c'est un argument
 cards.forEach((card) => {
@@ -36,6 +37,7 @@ for(let i = 0; i<cards.length; i++){
 function check(){
     if(counter==2){
         if(  cardPair[0].querySelector('img').src== cardPair[1].querySelector('img').src){
+            alert(srcs[0])
             matched()
         }
         else{
@@ -66,7 +68,6 @@ function unmatched(x,y){
     cardPair = []
 }
 function time(){
-    console.log('dans time(). score: ' + score);
 
     if (score <2){
         setTimeout( timer(), 1000);
@@ -82,7 +83,7 @@ function timer(){
         if(secs==60){secs=0;mins++}
     
         secs<10?SS=`0${secs}`:SS=`${secs}`
-        mins<10?MM=`0${mins}`:SS=`${mins}`
+        mins<10?MM=`0${mins}`:MM=`${mins}`
     
         document.querySelector('#time').innerHTML = `${MM}:${SS}`
         if(score != 2){ timer() }
@@ -92,9 +93,7 @@ function timer(){
 
 function shuffle(){
     let images = document.querySelectorAll('img')
-    let srcs = ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg', 'images/img4.jpg', 'images/img5.jpg', 'images/img6.jpg', 'images/img7.jpg', 'images/img8.jpg', 'images/img9.jpg', 'images/img10.jpg','images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg', 'images/img4.jpg', 'images/img5.jpg', 'images/img6.jpg', 'images/img7.jpg', 'images/img8.jpg', 'images/img9.jpg', 'images/img10.jpg'];
 //   'images/img11.jpg', 'images/img12.jpg', 'images/img13.jpg', 'images/img14.jpg', 'images/img15.jpg', 'images/img16.jpg', 'images/img17.jpg', 'images/img18.jpg', 'images/img19.jpg', 'images/img20.jpg',
-    
     for(let i=srcs.length-1; i>0; i--){
         let j = Math.floor(Math.random() * i)
         let temp = srcs[i]
@@ -105,4 +104,5 @@ function shuffle(){
     for(let i = 0; i<images.length; i++){
         images[i].src = srcs[i]
     }
+
 }
