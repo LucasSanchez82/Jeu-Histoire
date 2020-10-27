1c3ec3d191ae2bf1ce06315423c7db36e2f546a3
Lucas Sanchez <08.lucas.sanchez@gmail.com>
2020-10-27 18:14:15 +0100

    add imge=>prototype with bugs if we run ingame

--- memo-difficile/main.js
+++ memo-difficile/main.js
@@ -1,10 +1,16 @@
 let cards = document.querySelectorAll('.card_inner')
+let cardsOuter = document.querySelectorAll('.card_outer')
+let cardsFront = document.querySelectorAll('.front')
+let cardsBack = document.querySelectorAll('.back')
 let firstClick = false
 let counter = 0
 let cardPair = []
+let cardPairOuter = []
+let cardPairFront = []
+let cardPairBack  = []
 let score = document.querySelector("#score").innerHTML
-let srcs = ['IMG 1', 'IMG 2', 'IMG 3', 'IMG 4', 'IMG 5', 'IMG 6', 'IMG 7', 'IMG 8', 'IMG 9', 'IMG 10','IMG 1', 'IMG 2', 'IMG 3', 'IMG 4', 'IMG 5', 'IMG 6', 'IMG 7', 'IMG 8', 'IMG 9', 'IMG 10'];
-
+let srcs = ['Sacre De Napoleon', 'Sacre de Napoleon', '1er abdication de napoleon', '1er abdication de napoleon', 'bataille de ligny(belgique)', 'bataille de ligny(belgique)', 'bataille de waterlo', 'bataille de waterlo', 'fin du congres de Vienne', 'fin du congres de Vienne', 'fuite de Louis XIII', 'fuite de Louis XIII', 'mort de Napoleon a Saint-Helene', 'mort de Napoleon a Saint-Helene', 'ouverture du congres de Vienne', 'ouverture du congres de Vienne', "retour de napoleonde l'ile d'elbe", "retour de napoleonde l'ile d'elbe", 'seconde abdication de napoleon','seconde abdication de napoleon',  ];
+let tour=0
 // c'est bien juste card sans "s" dans le foreach, c'est un argument
 cards.forEach((card) => {
     card.state = 'unclicked'
@@ -20,8 +26,14 @@ for(let i = 0; i<cards.length; i++){
         if(cards[i].state == 'unclicked'){
             cards[i].style.transform = 'rotateY(180deg)'
             cards[i].state = 'clicked'
+
+            
+            
             counter++
+            cardPairOuter.push(cardsOuter[i])
             cardPair.push(cards[i])
+            cardPairFront.push(cardsFront[i])
+            cardPairBack.push(cardsBack[i])
             check()
         }
 
@@ -30,6 +42,7 @@ for(let i = 0; i<cards.length; i++){
             cards[i].state = 'unclicked'
             counter--
             cardPair = []
+
         }
     })
 }
@@ -37,8 +50,8 @@ for(let i = 0; i<cards.length; i++){
 function check(){
     if(counter==2){
         if(  cardPair[0].querySelector('img').src== cardPair[1].querySelector('img').src){
-            verification()
-           
+            verification()   
+            tour++        
         }
         else{
             unmatched(cardPair[0],cardPair[1])
@@ -59,35 +72,11 @@ function verification() {
     setTimeout(() => {
         let promptReply = prompt(`c\'est quoi/qui est illustré.e sur cette carte?  ||  ${srcs[0]} ||  ${srcs[1]} ||  ${srcs[2]} ||  ${srcs[3]} ||  ${srcs[4]} ||  ${srcs[5]} ||  ${srcs[6]} ||  ${srcs[7]} ||  ${srcs[8]} ||  ${srcs[9]} || REP${cardSrcName}` ).split(" ").join("").toLowerCase()
         if(promptReply == cardSrcName) {
-            scoreChangeMore() //quand on repond faux animation=>score+1
+            scoreChangeMore() //quand on repond vrai animation=>score+1
         }else{
             scoreChangeLess() //quand on repond faux animation=>score-1
         }
-    }, 0.76);
-}
-let i = 1
-let j = 450
-function scoreAnimation() {
-
-    if(i < 450){
-        
-        if(i<250){i = i*1.08}else{i = i*(262/252)}
-        
-        document.getElementById('scoreAnimation').style.right = i+ 'px';
-        document.getElementById('scoreAnimation').style.opacity = i/458;
-        j = 450
-        requestAnimationFrame(scoreAnimation)
-    }else if(j>1){
-        j = j*0.97
-        document.getElementById('scoreAnimation').style.right =  j +'px';
-        document.getElementById('scoreAnimation').style.opacity = j/277;
-        if(j>1){
-            requestAnimationFrame(scoreAnimation)
-        }else{
-            i=1
-        }
-    }
-
+    }, 500);
 }
 
 function scoreChangeMore() {
@@ -101,13 +90,47 @@ function scoreChangeLess() {
     document.getElementById('scoreAnimation').style.background = 'linear-gradient(45deg, #240000 0%, #5c1313 35%, #c77171 70%, #e69090 100%)';
     document.getElementById('scoreAnimation').innerHTML = '+0';
     requestAnimationFrame(scoreAnimation)
+
+    
+    cardPair[0].state = 'blocked'
+    cardPair[1].state = 'blocked'
+    counter = 0
+    cardPair = []
+    cardPairFront = []
+    cardPairBack = []
+    
+    cardPairOuter = []
 }
 
 function matched(){
     cardPair[0].state = 'blocked'
     cardPair[1].state = 'blocked'
-    counter = 0
-    cardPair = []
+/////////   \/     pour que les cartes disparraissent si reponse = vrai    \/    //////////// 
+    // cardPair[0].style.display = 'none';
+    // cardPair[1].style.display = 'none';
+
+    cardPair[0].style.animation = 'scaleNone 1s ease forwards';
+    cardPair[1].style.animation = 'scaleNone 1s ease forwards';
+    setTimeout(() => {
+        cardPairOuter[0].style.display = 'none';
+        cardPairOuter[1].style.display = 'none';
+        
+        cardPairFront[0].style.display = 'none';
+        cardPairBack[1].style.display = 'none';
+        
+        cardPairFront = []
+        cardPairBack = []
+        
+        cardPairOuter = []
+        cardPair = []
+        console.log('c ok');
+
+            counter = 0
+
+    }, 1000);
+    counter=0
+
+//////////////////////////                 /\                     /////////////////////////////////////
     
     score++
     document.querySelector('#score').innerHTML = score
@@ -122,11 +145,15 @@ function unmatched(x,y){
     cardPair[0].state = 'unclicked'
     cardPair[1].state = 'unclicked'
     counter = 0
+         cardPairFront = []
+        cardPairBack = []
+        
+        cardPairOuter = []
     cardPair = []
 }
 function time(){
 
-    if (score <10){
+    if (tour <10){
         setTimeout( timer(), 1000);
     }
 }
@@ -158,7 +185,37 @@ function shuffle(){
     }
     
     for(let i = 0; i<images.length; i++){
-        images[i].src = "images/"+srcs[i].split(" ").join("").toLowerCase() +".jpg"
+        images[i].src = "images/"+srcs[i].split(" ").join("").toLowerCase() +".png"
+    }
+
+}
+
+
+
+
+
+//////////////
+let i = 1
+let j = 450
+function scoreAnimation() {
+
+    if(i < 450){
+        
+        if(i<250){i = i*1.1}else{i = i*(262/252)}
+        
+        document.getElementById('scoreAnimation').style.right = i+ 'px';
+        document.getElementById('scoreAnimation').style.opacity = i/458;
+        j = 450
+        requestAnimationFrame(scoreAnimation)
+    }else if(j>1){
+        j = j*0.9
+        document.getElementById('scoreAnimation').style.right =  j +'px';
+        document.getElementById('scoreAnimation').style.opacity = j/800;
+        if(j>1){
+            requestAnimationFrame(scoreAnimation)
+        }else{
+            i=1
+        }
     }
 
 }
